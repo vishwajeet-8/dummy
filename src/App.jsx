2cff6fa622f035d49a70419ac072dcf1f5372968
vishwajeet-8 <vishwajeetrout8@gmail.com>
2025-05-13 00:44:16 +0530

    fix file name

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import DataAgents from "./components/DataAgents";
 import WorkflowResults from "./components/WorkflowResults";
 import Chatbot from "./components/Chatbot";
 import WorkflowData from "./components/WorkflowData";
-import Logs from "./components/logs/Logs";
+import Logs from "./components/logs/Logs.jsx";
 import { BarProvider } from "./context/bar/BarProvider";
 import { AgentProvider } from "./context/agent/AgentProvider";
 
